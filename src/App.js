b293352649c2a3f6da9bc743c3bc495881ae0f21
DeepakM02 <deepak.mittal119@gmail.com>
2020-12-31 03:02:47 +0530

    fix: render converted value after api call

--- src/App.js
+++ src/App.js
@@ -7,73 +7,12 @@ import { constant } from "./constant";
 import "./App.css";
 
 const App = () => {
-  const [fromCurrencyValue, setFromCurrency] = useState(1);
-  const [toCurrencyValue, setToCurrency] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState(1);
+  const [toCurrency, setToCurrency] = useState("");
   const [fromCountry, setFromCountry] = useState("USD");
   const [toCountry, setToCountry] = useState("INR");
   const [convertedText, setConvertedText] = useState("");
   const [graphData, setGraphData] = useState("");
-
-  useEffect(() => {
-    convertCurrency();
-  }, []);
-
-  // currency conversion
-  const convertCurrency = () => {
-    const url = `${constant.base_url}/latest?base=${fromCountry}&&symbols=${toCountry}`;
-
-    fetch(url)
-      .then((res) => res.json())
-      .then((response) => {
-        const convertedValue = response.rates[toCountry];
-        const value = (fromCurrencyValue * convertedValue).toFixed(2);
-        setToCurrency(value);
-        changeResultText(convertedValue);
-        getHistorialRates();
-      })
-      .catch((err) => {
-        alert(err);
-      });
-  };
-
-  const blockInvalidChar = (e) =>
-    ["e", "E", "+", "-"].includes(e.key) && e.preventDefault();
-
-  // change result after getting converted value
-  const changeResultText = (convertedValue) => {
-    const fromC = currencyList.filter((e) => e.abbr === fromCountry)[0].name;
-    const toC = currencyList.filter((e) => e.abbr === toCountry)[0].name;
-    const text = `1 ${fromC} = ${convertedValue.toFixed(2)} ${toC}`;
-    setConvertedText(text);
-  };
-
-  // get historical rates
-  const getHistorialRates = () => {
-    const today = new Date();
-    const [year, month, day] = [
-      today.getFullYear(),
-      today.getMonth() + 1,
-      today.getDate(),
-    ];
-    const start_at = `${year - 1}-${month}-${day}`;
-    const end_at = `${year}-${month}-${day}`;
-    const url = `${constant.base_url}/history?start_at=${start_at}&end_at=${end_at}&symbols=${fromCountry}&base=${toCountry}`;
-
-    fetch(url)
-      .then((res) => res.json())
-      .then((response) => {
-        const graphData = [
-          {
-            specialLabel: "Historical Rates",
-            data: Object.entries(response.rates).map(([key, value]) => [
-              new Date(key),
-              value[fromCountry],
-            ]),
-          },
-        ];
-        setGraphData(graphData);
-      });
-  };
 
   // Chart axes config
   const axes = useMemo(
@@ -93,7 +32,73 @@ const App = () => {
     []
   );
 
-  
+  useEffect(() => {
+    convertCurrency();
+  }, []);
+
+  // currency conversion
+  const convertCurrency = () => {
+    const url = `${constant.base_url}/latest?base=${fromCountry}&&symbols=${toCountry}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((response) => {
+        const toCurrencyValue = response.rates[toCountry];
+        const value = (fromCurrency * toCurrencyValue).toFixed(2);
+        setToCurrency(`${value} ${toCountry}`);
+        changeResultText(toCurrencyValue);
+        getHistorialRates();
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  const blockInvalidChar = (e) =>
+    ["e", "E", "+", "-"].includes(e.key) && e.preventDefault();
+
+  // change result after getting converted value
+  const changeResultText = (convertedValue) => {
+    const fromC = currencyList.filter((e) => e.abbr === fromCountry)[0].name;
+    const toC = currencyList.filter((e) => e.abbr === toCountry)[0].name;
+    const text = `1 ${fromC} = ${convertedValue.toFixed(2)} ${toC}`;
+    setConvertedText(text);
+  };
+
+  const getStartAtEndAt = () => {
+    const today = new Date();
+    const [year, month, day] = [
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate(),
+    ];
+    const start_at = `${year - 1}-${month}-${day}`;
+    const end_at = `${year}-${month}-${day}`;
+
+    return { start_at, end_at };
+  };
+
+  // get historical rates
+  const getHistorialRates = () => {
+    const { start_at, end_at } = getStartAtEndAt();
+    const url = `${constant.base_url}/history?start_at=${start_at}&end_at=${end_at}&symbols=${fromCountry}&base=${toCountry}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((response) => {
+        const graphData = [
+          {
+            specialLabel: "Historical Rates",
+            data: Object.entries(response.rates).map(([key, value]) => [
+              new Date(key),
+              value[fromCountry],
+            ]),
+          },
+        ];
+        setGraphData(graphData);
+      });
+  };
+
   return (
     <div className="App">
       <h1>Currency Converter</h1>
@@ -102,8 +107,8 @@ const App = () => {
         <input
           type="number"
           min={1}
-          name="fromCurrencyValue"
-          value={fromCurrencyValue}
+          name="fromCurrency"
+          value={fromCurrency}
           onKeyDown={blockInvalidChar}
           onChange={(e) => setFromCurrency(e.target.value)}
         />
@@ -133,17 +138,13 @@ const App = () => {
           ))}
         </select>
 
-        <button disabled={!fromCurrencyValue} onClick={convertCurrency}>
+        <button disabled={!fromCurrency} onClick={convertCurrency}>
           Convert
         </button>
       </div>
 
       <div className="result-section">
-        {toCurrencyValue ? (
-          <h2>
-            {toCurrencyValue} {toCountry}
-          </h2>
-        ) : null}
+        {toCurrency ? <h2>{toCurrency}</h2> : null}
         <h3>{convertedText}</h3>
       </div>
 
